43db776799138438c1c5e344620b55fa69394606
sangzun-han <hsjun1996@naver.com>
2023-01-13 23:46:08 +0900

    ADD : 10-side-effects-recuders-context-api

---
+++ 10-side-effest-reducers-context-api/src/store/auth-context.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import React from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInfomation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInfomation === "1") setIsLoggedIn(true);
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
