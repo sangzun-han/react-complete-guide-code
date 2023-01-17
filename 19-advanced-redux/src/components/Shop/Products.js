ea0d668745169a25c767d2c5262402b51e1f6cfa
sangzun-han <hsjun1996@naver.com>
2023-01-18 07:50:33 +0900

    ADD : 19-advanced-redux

---
+++ 19-advanced-redux/src/components/Shop/Products.js
@@ -0,0 +1,37 @@
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "책",
+    description: "젤 처음 쓴 책",
+  },
+  {
+    id: "p2",
+    price: 3,
+    title: "책 2번째",
+    description: "두번째 쓴 책",
+  },
+];
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
