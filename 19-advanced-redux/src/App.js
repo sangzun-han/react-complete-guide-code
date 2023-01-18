370c2baa3357bdba882d4c659a7ea68eccc2c2fa
한상준 <hsjun1996@naver.com>
2023-01-18 17:56:27 +0900

    ADD : 19-advanced-redux

--- 19-advanced-redux/src/App.js
+++ 19-advanced-redux/src/App.js
@@ -1,15 +1,70 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
+
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  useEffect(() => {
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://react-http-6b4ab.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
 
+      if (!response.ok) {
+        throw new Error("데이터 보내기 실패");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "성공.!",
+          message: "sending cart data 성공",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "에러!",
+          message: "sending cart data 실패",
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
